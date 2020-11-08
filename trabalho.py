8fea1def92274943f3229674c229ad36cc3a6b55
Diego Cacau <dc_cacau@hotmail.com>
2020-11-08 10:52:34 -0200

    todas as funcoes funcionando

--- trabalho.py
+++ trabalho.py
@@ -2,9 +2,9 @@ import socket
 from struct import unpack
 from urllib.parse import urlparse
 import sys
+import os
 
-
-class Resquest():
+class ResquestHTML():
 	def __init__(self, url):
 		self.header_delimiter = b'\r\n\r\n'
 		self.end_site_delimiter = "\r\n0\r\n"
@@ -32,6 +32,8 @@ class Resquest():
 		self.client.send(request.encode())
 
 		self.get_data()
+
+		self.client.close()
 
 		return self.html
 
@@ -110,14 +112,173 @@ class Resquest():
 			header_line = line.split(": ")
 			self.headers[header_line[0]] = header_line[1]
 
+	def get_host(self):
+		return self.target_host
 
 
 
+
+
+
+
+
+
+
+class ResquestIMG():
+	def __init__(self, host, path):
+		self.header_delimiter = b'\r\n\r\n'
+		self.is_first_request = True
+		self.headers = {}
+		self.img = ""
+		self.total_size = 0
+		self.actual_size = 0
+		self.chunked_size = False
+		self.should_stop = False
+		
+		self.target_host = host
+		self.path = path
+
+		self.define_socket()
+
+	def define_socket(self):
+		target_port = 80
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+		self.client.connect((self.target_host,target_port)) 
+
+	def get(self):
+		request = "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n" % (self.path, self.target_host)
+		self.client.send(request.encode())
+
+		self.get_data()
+
+		self.client.close()
+
+		if(self.should_stop):
+			return b""
+
+		return self.img
+
+	def get_data(self):
+		response = self.client.recv(1024)
+
+		self.parse_data(response)
+
+		if(self.should_stop):
+			return
+
+		if(not self.chunked_size):
+			print(self.total_size, self.actual_size)
+			print("\n\n")
+			if(self.total_size > self.actual_size):
+				self.get_data()
+		else:
+			if(response):
+				self.get_data()
+
+
+	def parse_data(self, response):
+		if(self.is_first_request):
+			try:
+				index = response.index(self.header_delimiter)
+			except:
+				self.should_stop = True
+				return
+
+			self.parse_headers(response[:index])
+
+			if(self.status != 200):
+				print("HTTP estatus diferente de 200")
+				print("Valor recebido: ", self.status)
+				self.should_stop = True
+				return
+
+			self.img = response[index+4:]
+
+			print(self.img)
+			print("\n\n")
+
+			try:
+				self.total_size = int(self.headers["Content-Length"])
+			except:
+				self.chunked_size = True
+
+			self.actual_size = len(response[index:])
+
+			self.is_first_request = False
+		else:
+			self.img = self.img + response
+			
+			if(not self.chunked_size):
+				self.actual_size = self.actual_size + len(response)
+
+	def parse_headers(self, headers):
+		data = headers.decode("latin-1")
+
+		status_line = True
+		for line in data.split("\r\n"):
+
+			if(status_line):
+				self.status = int(line.split(" ")[1])
+				status_line = False
+				continue
+			header_line = line.split(": ")
+			self.headers[header_line[0]] = header_line[1]
+		print(self.headers)
+		print("\n\n")
+
+
+
+
+
+with open('temp.txt') as html:
+    content = html.read()
+    for line in content.split("\n"):
+    	print(line)
+    	try:
+    		os.remove(line)
+    	except:
+    		pass
+
+sys.exit()
 
 url = 'http://www.ic.uff.br/index.php/pt/'
-request = Resquest(url)
-html = request.get()
+request = ResquestHTML(url)
+# html = request.get()
+
+# print(html)
+# with open("pagina.html", "w") as file:
+# 	file.write(html)
 
 
-with open("pagina.html", "w") as file:
-	file.write(html)
+import re
+content = ""
+with open('pagina.html') as html:
+    content = html.read()
+    pat = re.compile (r'<img [^>]*src="([^"]+)')
+    matches = pat.findall(content)
+
+
+print(matches)
+print("\n\n")
+
+
+temp_names = []
+for img in matches:
+	print(img)
+	name = os.path.basename(img)
+	temp_names.append(name)
+	# request2 = ResquestIMG(request.get_host(), img)
+	# img_content = request2.get()
+
+	# with open(name, 'wb') as file_to_write:
+	# 	file_to_write.write(img_content)
+
+	content = content.replace(img, name)
+
+
+with open("pagina2.html", "w") as file:
+	file.write(content)
+
+with open("temp.txt", "w") as file:
+	for name in temp_names:
+		file.write(name + "\n")
